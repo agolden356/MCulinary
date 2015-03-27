4e0e048a8b3cf24dcc1f8bdd9c83f41fb713ca93
agolden356 <aaronrobertgolden@gmail.com>
2015-03-27 03:10:24 -0700

    a

--- js/grayscale.js
+++ js/grayscale.js
@@ -41,7 +41,7 @@ function init() {
         zoom: 15,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(33.808056, -118.110494), // Millikan High School
+        center: new google.maps.LatLng(33.808080, -118.110618), // Millikan High School
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
@@ -169,7 +169,7 @@ function init() {
 
     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
     var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(33.808056, -118.110494);
+    var myLatLng = new google.maps.LatLng(33.808080, -118.110618);
     var beachMarker = new google.maps.Marker({
         position: myLatLng,
         map: map,
